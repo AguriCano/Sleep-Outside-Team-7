8ea7537a3e912069673a58fbf2ac1e2ac84db8f6
sundazekiks <abutontravis1@gmail.com>
2026-01-20 13:12:45 -0500

    added the feature custom alert box

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { renderListWithTemplate, updateCartCounter } from "./utils.mjs";
 import { productCardTemplate } from "./product-card-temp.mjs";
-
+import Alert from "./Alert.js";
 // Initialize cart counter on page load
 updateCartCounter();
 
@@ -12,6 +12,13 @@ const productList = new ProductList("tents", productData, document.querySelector
 productList.init().then(products => {
   renderListWithTemplate(productCardTemplate, document.querySelector(".product-list"), products, "beforeend", true);
 })
+
+const a = new Alert(document.getElementById("atemp"), document.getElementById("alertHolder"))
+
+a.generateAlert()
+
+
+
 
 
 
