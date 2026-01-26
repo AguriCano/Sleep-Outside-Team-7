c5971fdc490da8cbb6bc91eb6d6e9915c27053e3
acottam <adam@gofishcreative.com>
2026-01-25 20:05:17 -0800

    W03 Stretch Activity Added - Cart Item Fixed

--- src/js/product.js
+++ src/js/product.js
@@ -2,11 +2,10 @@ import { getParams, updateCartCounter, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-// Load header and footer
-loadHeaderFooter();
-
-// Initialize cart counter on page load
-updateCartCounter();
+// Load header and footer, then update cart counter
+loadHeaderFooter().then(() => {
+  updateCartCounter();
+});
 
 const dataSource = new ProductData("tents");
 const productId = getParams(window.location.search);
